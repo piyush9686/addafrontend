0b3d128944c6153dcb72b4b84d0ab4fd0c555db3
PIYUSH <piyushagrawal0213@gmail.com>
2026-04-08 13:39:16 +0530

    fix: correct import path for loadRazorpay utility

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import api from "../services/api.js";
-import loadRazorpay from "../utils/loadRazorpay.js";
+import loadRazorpay from "../utils/loadRazorPay.js";
 
 const Checkout = () => {
 
